caf816c4f9bb0b02f6c80bf3b94033a5b76b0608
Ricardo Santos <santos.ricardo@bcgplatinion.com>
2021-06-17 10:45:13 +0200

    change name of directory in bucket for state

--- src/eks/backend.tf
+++ src/eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-gitlab-remote-state"
-    key    = "gh_actions/terraform.tfstate"
+    key    = "gitlab_eks/terraform.tfstate"
     region = "eu-central-1"
   }
 }
